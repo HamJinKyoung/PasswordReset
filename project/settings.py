16dc1abe0e93748411d7c452e37340f24279b617
HamJinkyoung <wlsrud3803@gmail.com>
2020-09-13 16:08:24 +0900

    개인이메일주소,비밀번호 삭제

--- project/settings.py
+++ project/settings.py
@@ -131,9 +131,9 @@ EMAIL_HOST = 'smtp.gmail.com'
 # 메일을 호스트하는 서버
 EMAIL_PORT = '587'
 # gmail과의 통신하는 포트
-EMAIL_HOST_USER = 'wlsrud3803@gmail.com'
+EMAIL_HOST_USER = '발신할 이메일'
 # 발신할 이메일
-EMAIL_HOST_PASSWORD = 'wlsrud3803'
+EMAIL_HOST_PASSWORD = '발신할 메일의 비밀번호'
 # 발신할 메일의 비밀번호
 EMAIL_USE_TLS = True
 # TLS 보안 방법
